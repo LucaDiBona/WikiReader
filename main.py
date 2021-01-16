49c29a5ae2c6fd617993082d566e7cbf790e5b19
LucaDiBona <lucadibonasutton@gmail.com>
2021-01-16 17:06:58 +0000

    infoBar() can now process inputs that would render over more than one line onto a single line without issues

--- main.py
+++ main.py
@@ -16,15 +16,21 @@ curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
-def infobar(currentInfoBar, page, section):
+def infobar(page, section):
     num_rows, num_cols = screen.getmaxyx()
-    while len(currentInfoBar) < num_cols:
-        currentInfoBar += " "
-    return currentInfoBar
+    infoBar = page + " | " + section
+    while len(infoBar) < num_cols:
+        infoBar += " "
+    if len(infoBar) > num_cols:
+        abbrvLength = num_cols + len(section) - len(infoBar) - 3        #appropriate length for new section to display (not including ...)
+        abbrvSection = section[:abbrvLength]
+        abbrvSection += "..."
+        infoBar = infobar(page, abbrvSection)
+    return infoBar
 
-screen.addstr(0, 0, infobar("This is the current infobar", None, None), curses.A_STANDOUT)
+screen.addstr(0, 0, infobar(currentPage, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."), curses.A_STANDOUT)
 screen.addstr(1, 0, page.summary)
-
+screen.addstr(0,0,"")
 screen.refresh()
 curses.napms(4000)
 
@@ -33,5 +39,5 @@ screen.keypad(False)
 curses.echo()
 curses.endwin()
 
-print(text)
-print(len(text))
+##print(text)
+##print(len(text))
