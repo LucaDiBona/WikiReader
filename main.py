73b25a0282e5cc8553a2bae20ac4395082ab0ca1
LucaDiBona <lucadibonasutton@gmail.com>
2021-01-16 16:01:20 +0000

    Added infobar

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ import curses, wikipedia
 This code displays the wikipedia page for python in a terminal window.
 
 """
-text=wikipedia.summary("Python (programming langauge)")
+currentPage = "Python (programming langauge)"
+
+page=wikipedia.page(currentPage, None, True, True, True)
+text=page.content
+
 
 screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
-screen.addstr(0,0,text)
+def infobar(currentInfoBar, page, section):
+    num_rows, num_cols = screen.getmaxyx()
+    while len(currentInfoBar) < num_cols:
+        currentInfoBar += " "
+    return currentInfoBar
+
+screen.addstr(0, 0, infobar("This is the current infobar", None, None), curses.A_STANDOUT)
+screen.addstr(1, 0, page.summary)
 
 screen.refresh()
 curses.napms(4000)
@@ -21,3 +32,6 @@ curses.nocbreak()
 screen.keypad(False)
 curses.echo()
 curses.endwin()
+
+print(text)
+print(len(text))
