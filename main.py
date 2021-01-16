8101e10a9818d478bd653903b5faa3acb2479f02
LucaDiBona <lucadibonasutton@gmail.com>
2021-01-16 15:24:34 +0000

    Display Python summary

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import curses, wikipedia
 This code displays the wikipedia page for python in a terminal window.
 
 """
+text=wikipedia.summary("Python (programming langauge)")
 
 screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
+screen.addstr(0,0,text)
+
+screen.refresh()
 curses.napms(4000)
 
 curses.nocbreak()
