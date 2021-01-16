5f119321e56e324ce4f574e13ef2c47a69bf0628
LucaDiBona <lucadibonasutton@gmail.com>
2021-01-16 15:15:28 +0000

    Added curses initialisation

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import wikipedia, urwid
+import curses, wikipedia
+
+""" 
+
+This code displays the wikipedia page for python in a terminal window.
+
+"""
+
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+
+curses.napms(4000)
+
+curses.nocbreak()
+screen.keypad(False)
+curses.echo()
+curses.endwin()
